cc61f270cee5daff7ac31650350d52f39cf2b391
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-05-10 15:02:19 -0700

    testing json

--- js/pilliars.js
+++ js/pilliars.js
@@ -1,9 +1,9 @@
 
 
 function load(){
-  JSONArray obj = newASONArray(items.toString());
-  for(int i=0; i<obj.lenth();i++) {
-    JSONObject jsonobj = obj.getJSONObject(i);
+  // JSONArray obj = newASONArray(items.toString());
+  for(int i=0; i<items.lenth();i++) {
+    JSONObject jsonobj = items.getJSONObject(i);
     document.GetElementById("bill").innerHTML = jsonobj.getString("name");
     document.GetElementById("company").innerHTML = jsonobj.getString("company.name");
   }
