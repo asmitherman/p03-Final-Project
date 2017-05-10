7701957a0a105f36f5dab9c2bf032f447ecaa758
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-05-10 14:35:36 -0700

    testing json

--- js/pilliars.js
+++ js/pilliars.js
@@ -1,15 +1,4 @@
 
-$.getJSON( "https://asmitherman.github.io/p03-Final-Project/billionaires.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
-
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
 
 
 function load () {
