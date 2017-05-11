16ac9f0723ff2c22ac51a15f65d5ecc03c644f1a
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-05-10 19:22:51 -0700

    testing json

--- js/pilliars.js
+++ js/pilliars.js
@@ -4,19 +4,19 @@ function load(){
   $.getJSON( "https://asmitherman.github.io/p03-Final-Project/js/billionaires.json", function( data ) {
     var items = [];
     $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
+      items.push( "<li id='" + key + "'>" + val.name + "</li>" );
     });
 
-  //   $( "<ul/>", {
-  //     "class": "my-new-list",
-  //     html: items.join( "" )
-  //   }).appendTo( "body" );
-  // });
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
   // JSONArray obj = newASONArray(items.toString());
   for(var i=0; i<items.lenth();i++) {
-    JSONObject jsonobj = items.getJSONObject(i);
-    document.GetElementById("bill").innerHTML = jsonobj.getString("name");
-    document.GetElementById("company").innerHTML = jsonobj.getString("company.name");
+    items[i];
+    // document.GetElementById("bill").innerHTML = jsonobj.getString("name");
+    // document.GetElementById("company").innerHTML = jsonobj.getString("company.name");
   }
     // String comp_name = jsonobj.getString("company.name");
   }
